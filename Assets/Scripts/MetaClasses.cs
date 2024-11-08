93185360ceec3b2a8cb69b6a298a0e9438bb2858
Srinivas <srinivasktss@gmail.com>
2024-11-08 09:54:41 +0530

    Created Symbol script

---
+++ Assets/Scripts/MetaClasses.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace MysticalForestAdventure
+{
+    public enum Symbol
+    {
+        LOW1, LOW2, LOW3, LOW4, LOW5,
+        HIGH1, HIGH2, HIGH3, HIGH4,
+        WILD,
+        BONUS,
+        SCATTER
+    }
+}
